098a0c9af62a80f7a174d0b94e820584b03abf16
fcying <fcyingmk2@gmail.com>
2018-11-25 12:01:51 +0800

    modify add-apt-repository script

--- build/vim_build.sh
+++ build/vim_build.sh
@@ -61,7 +61,7 @@ if [ $install == "true" ]; then
             echo "os MANJARO"
         else
             echo "os Ubuntu/debian"
-            sudo apt install build-essential libncurses5-dev libxt-dev \
+            sudo apt install -y build-essential libncurses5-dev libxt-dev \
                 python3-dev python2.7-dev lua5.2 liblua5.2-dev
             #sudo apt install libgnome2-dev libgnomeui-dev libgtk2.0-dev libcairo2-dev libx11-dev libxpm-dev libatk1.0-dev
         fi

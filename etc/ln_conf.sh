45be8bcd87901c1dbe8a845dd586f36af28e83e5
fcying <fcyingmk2@gmail.com>
2021-05-05 11:04:55 +0800

    update ln_conf.sh

--- etc/ln_conf.sh
+++ etc/ln_conf.sh
@@ -3,25 +3,29 @@
 cd $(dirname $0)
 config_dir=$PWD
 
-if [ -f $HOME/.zshrc.local ]; then
-    sed -i "/CONFIG_DIR/d" $HOME/.zshrc.local
-fi
-echo "CONFIG_DIR=$config_dir" | tee -a $HOME/.zshrc.local
-ln -sfv $PWD/zshrc ~/.zshrc
-ln -sfv $PWD/bashrc ~/.bashrc
-ln -sfv $PWD/dircolors ~/.dircolors
-ln -sfv $PWD/inputrc ~/.inputrc
-ln -sfv $PWD/tigrc ~/.tigrc
-ln -sfv $PWD/ctags ~/.ctags
-ln -sfv $PWD/tmux.conf ~/.tmux.conf
-ln -sfv $PWD/npmrc ~/.npmrc
+app=${app:-"all"}
 
-# nvim
-mkdir -p ~/.config/nvim
-echo "set nvim init.vim"
-#rm -f ~/.config/nvim/init.vim
-#echo "source ~/.vim/vimrc" > ~/.config/nvim/init.vim
-ln -svf $PWD/../vimrc ~/.config/nvim/init.vim
+if [[ $app == "all" ]]; then
+    if [ -f $HOME/.zshrc.local ]; then
+        sed -i "/CONFIG_DIR/d" $HOME/.zshrc.local
+    fi
+    echo "CONFIG_DIR=$config_dir" | tee -a $HOME/.zshrc.local
+    ln -sfv $PWD/zshrc ~/.zshrc
+    ln -sfv $PWD/bashrc ~/.bashrc
+    ln -sfv $PWD/dircolors ~/.dircolors
+    ln -sfv $PWD/inputrc ~/.inputrc
+    ln -sfv $PWD/tigrc ~/.tigrc
+    ln -sfv $PWD/ctags ~/.ctags
+    ln -sfv $PWD/tmux.conf ~/.tmux.conf
+    ln -sfv $PWD/npmrc ~/.npmrc
+
+    # nvim
+    mkdir -p ~/.config/nvim
+    echo "set nvim init.vim"
+    #rm -f ~/.config/nvim/init.vim
+    #echo "source ~/.vim/vimrc" > ~/.config/nvim/init.vim
+    ln -svf $PWD/../vimrc ~/.config/nvim/init.vim
+fi
 
 # windows
 #mkdir %userprofile%\AppData\Local\nvim
@@ -29,15 +33,19 @@ ln -svf $PWD/../vimrc ~/.config/nvim/init.vim
 #echo source d:\tool\vim\vimrc > %userprofile%\_vimrc   #use this doesn't load defaults.vim
 
 # npm
-#type npm >/dev/null 2>&1 && {
-#    mkdir -p ~/.npm
-#    cd ~/.npm
-#    npm install cnpm
-#}
+if [[ $app == "npm" ]]; then
+    type npm >/dev/null 2>&1 && {
+        mkdir -p ~/.npm
+        cd ~/.npm
+        npm install cnpm
+    }
+fi
 
 # pypi mirror
-PYPI=https://opentuna.cn/pypi/web/simple
-pip3 install -i $PYPI pip -U
-pip3 config set global.index-url $PYPI
+if [[ $app == "pip" ]]; then
+    PYPI=https://opentuna.cn/pypi/web/simple
+    pip3 install -i $PYPI pip -U
+    pip3 config set global.index-url $PYPI
+fi
 
 
